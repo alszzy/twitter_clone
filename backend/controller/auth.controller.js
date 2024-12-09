947d662c78123da0d0ca19acb165234f95a8e712
alszzy <alsherman99@gmail.com>
2024-12-08 22:54:50 -0800

    project setup

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+    res.json({ task: "please sign up"})
+}
+export const login = async (req, res) => {
+    res.json({ task: "please sign up"})
+}
+export const logout = async (req, res) => {
+    res.json({ task: "please sign up"})
+}
